eed51ecf798f48f21c8441950214794af0f71ec0
FABIO A. OLIVEIRA <fabolive@us.ibm.com>
2015-10-13 18:01:02 -0400

    Testing changes to compile.

---
+++ bin/logstash-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p /opt
+pushd /opt
+  wget -nv https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.tar.gz
+  tar xzf logstash-1.4.2.tar.gz
+  mv logstash-1.4.2 logstash
+  rm logstash-1.4.2.tar.gz
+popd
+ls -l
