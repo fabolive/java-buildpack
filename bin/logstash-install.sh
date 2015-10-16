57cfac2d071d4ecb819b31d230bba66ecc754a40
FABIO A. OLIVEIRA <fabolive@us.ibm.com>
2015-10-16 11:26:46 -0400

    Testing a custom buildpack

--- bin/logstash-install.sh
+++ bin/logstash-install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+
+build_dir=$1
 my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+cp $my_dir/logstash*.sh $build_dir
 
-mkdir -p /home/vcap/app/opt
-cp $my_dir/logstash*.sh /home/vcap/app
-
-pushd /home/vcap/app/opt
+pushd $build_dir
   echo "Downloading and installing Logstash."
   wget -nv https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.tar.gz
   tar xzf logstash-1.4.2.tar.gz
   mv logstash-1.4.2 logstash
   rm logstash-1.4.2.tar.gz
 popd
+
+ls -lR $build_dir
