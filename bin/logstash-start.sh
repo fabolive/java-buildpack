ea09512a713da86fedd9eb4bda39ab3680abce22
FABIO A. OLIVEIRA <fabolive@us.ibm.com>
2015-10-16 12:15:53 -0400

    Testing a custom buildpack

--- bin/logstash-start.sh
+++ bin/logstash-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-JAVA_HOME="/home/vcap/app/.java-buildpack/open_jdk_jre"
+export JAVA_HOME="/home/vcap/app/.java-buildpack/open_jdk_jre"
 /home/vcap/app/logstash/bin/logstash agent -f /home/vcap/app/etc/logstash/conf.d &
