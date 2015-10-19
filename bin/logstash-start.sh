21f0c16f9fee2fb76f0d17358ed47d9735841a75
FABIO A. OLIVEIRA <fabolive@us.ibm.com>
2015-10-19 14:29:51 -0400

    Testing a custom buildpack

--- bin/logstash-start.sh
+++ bin/logstash-start.sh
@@ -8,4 +8,4 @@ echo "Tomcat Log Files:"
 ls -l /home/vcap/app/.java-buildpack/tomcat/logs
 
 export JAVA_HOME="/home/vcap/app/.java-buildpack/open_jdk_jre"
-/home/vcap/app/logstash/bin/logstash agent -f /home/vcap/app/etc/logstash/conf.d &
+/home/vcap/app/logstash/bin/logstash agent -f /home/vcap/app/etc/logstash/conf.d --debug &
