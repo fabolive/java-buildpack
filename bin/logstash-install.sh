62e64afc3dd827269302b30ec5d57821a93f3792
FABIO A. OLIVEIRA <fabolive@us.ibm.com>
2015-10-19 14:14:51 -0400

    Testing a custom buildpack

--- bin/logstash-install.sh
+++ bin/logstash-install.sh
@@ -4,6 +4,8 @@
 build_dir=$1
 my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+cp $my_dir/logstash*.sh $build_dir
+
 pushd $build_dir
   echo "Downloading and installing Logstash."
   wget -nv https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.tar.gz
--- bin/logstash-start.sh
+++ bin/logstash-start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-#ls -l /home/vcap/app/.java-buildpack/tomcat/conf
+ls -l /home/vcap/app/.java-buildpack/tomcat/conf
 cat /home/vcap/app/.java-buildpack/tomcat/conf/server.xml
 
-#echo "Tomcat Log Files:"
-#ls -l /home/vcap/app/.java-buildpack/tomcat/logs
+echo "Tomcat Log Files:"
+ls -l /home/vcap/app/.java-buildpack/tomcat/logs
 
-#export JAVA_HOME="/home/vcap/app/.java-buildpack/open_jdk_jre"
-#/home/vcap/app/logstash/bin/logstash agent -f /home/vcap/app/etc/logstash/conf.d &
+export JAVA_HOME="/home/vcap/app/.java-buildpack/open_jdk_jre"
+/home/vcap/app/logstash/bin/logstash agent -f /home/vcap/app/etc/logstash/conf.d &
