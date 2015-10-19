2fca36deadd124067ae4f049da4eb13b41e87751
FABIO A. OLIVEIRA <fabolive@us.ibm.com>
2015-10-19 15:05:42 -0400

    Added Logstash configuration to parse the customized Tomcat access log file and exceptions.

--- bin/logstash-install.sh
+++ bin/logstash-install.sh
@@ -4,6 +4,8 @@
 build_dir=$1
 my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+LOGSTASH_CONF_DIR=$build_dir/etc/logstash/conf.d
+
 cp $my_dir/logstash*.sh $build_dir
 
 pushd $build_dir
@@ -15,24 +17,14 @@ pushd $build_dir
   pwd
   
   echo "Copying customized server.xml file to Tomcat distribution"
-  cp $my_dir/../custom-server.xml .java-buildpack/tomcat/conf/server.xml 
+  cp $my_dir/logstash-related-config/custom-server.xml .java-buildpack/tomcat/conf/server.xml 
 popd
 
-mkdir -p $build_dir/etc/logstash/conf.d
-pushd $build_dir/etc/logstash/conf.d
-  cat >"111-input.conf" <<EOF
-input {
-  file {
-    path => '/home/vcap/app/.java-buildpack/tomcat/logs/**/*'
-  }
-}
-EOF
-
-
-  cat >"900-output.conf" <<EOF
-output {
-  stdout {
-  }
-}
-EOF
-popd
+echo "Generating Logstash configuration"
+mkdir -p $LOGSTASH_CONF_DIR
+cp $my_dir/../logstash-related-config/logstash-tomcat.template $LOGSTASH_CONF_DIR/logstash-tomcat.conf
+sed -i s"TOMCAT_SERVER_DIR/\/home\/vcap\/app\/.java-buildpack\/tomcat/" $LOGSTASH_CONF_DIR/logstash-tomcat.conf
+sed -i s"CA_TAG/$CA_TAG/" $LOGSTASH_CONF_DIR/logstash-tomcat.conf
+sed -i s"ES_HOST/$ES_HOST/" $LOGSTASH_CONF_DIR/logstash-tomcat.conf
+sed -i s"ES_PORT/$ES_PORT/" $LOGSTASH_CONF_DIR/logstash-tomcat.conf
+sed -i s"INDEX/$ES_INDEX/" $LOGSTASH_CONF_DIR/logstash-tomcat.conf
