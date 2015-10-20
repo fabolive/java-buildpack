f9b526425a12a40f7561339207e606dfce4177f1
FABIO A. OLIVEIRA <fabolive@us.ibm.com>
2015-10-19 23:02:16 -0400

    Limit Logstash heap size

--- bin/logstash-start.sh
+++ bin/logstash-start.sh
@@ -8,4 +8,5 @@ echo "Tomcat Log Files:"
 ls -l /home/vcap/app/.java-buildpack/tomcat/logs
 
 export JAVA_HOME="/home/vcap/app/.java-buildpack/open_jdk_jre"
+export LS_HEAP_SIZE=256m
 /home/vcap/app/logstash/bin/logstash agent -f /home/vcap/app/etc/logstash/conf.d --debug &
