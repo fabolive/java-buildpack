35667df720ee22079d3a6e91c0cb6580bcdc417e
FABIO A. OLIVEIRA <fabolive@us.ibm.com>
2015-10-27 16:54:17 -0400

    Tests for collectd

--- bin/logstash-install.sh
+++ bin/logstash-install.sh
@@ -9,13 +9,24 @@ LOGSTASH_CONF_DIR=$build_dir/etc/logstash/conf.d
 cp $my_dir/logstash*.sh $build_dir
 
 pushd $build_dir
-  echo "Downloading and installing Logstash."
+  echo "Downloading and installing Logstash"
   wget -nv https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.tar.gz
   tar xzf logstash-1.4.2.tar.gz
   mv logstash-1.4.2 logstash
   rm logstash-1.4.2.tar.gz
   echo "Copying customized server.xml file to Tomcat distribution"
-  cp $my_dir/../logstash-related-config/custom-server.xml .java-buildpack/tomcat/conf/server.xml 
+  cp $my_dir/../logstash-related-config/custom-server.xml .java-buildpack/tomcat/conf/server.xml
+
+  echo "Downloading collectd"
+  echo "Build dir = $build_dir"
+  ls -l $build_dir
+  which gcc
+  which make
+  wget https://collectd.org/files/collectd-5.4.2.tar.gz
+  tar xzf collectd-5.4.2.tar.gz
+#  cd collectd-5.4.2
+#  echo "Compiling collectd"
+#  ./configure --prefix=/root/test --exec-prefix=/root/test --bindir=/root/test/bin
 popd
 
 echo "Generating Logstash configuration"
@@ -28,3 +39,8 @@ sed -i s"/ES_PORT/$ES_PORT/" $LOGSTASH_CONF_DIR/logstash-tomcat.conf
 sed -i s"/INDEX/$ES_INDEX/" $LOGSTASH_CONF_DIR/logstash-tomcat.conf
 
 cat $LOGSTASH_CONF_DIR/logstash-tomcat.conf
+
+echo "Generating collectd configuration"
+cp $my_dir/../logstash-related-config/collectd.template $build_dir/etc/collectd.conf
+
+cat $build_dir/etc/collectd.conf
--- bin/logstash-start.sh
+++ bin/logstash-start.sh
@@ -7,6 +7,11 @@ cat /home/vcap/app/.java-buildpack/tomcat/conf/server.xml
 echo "Tomcat Log Files:"
 ls -l /home/vcap/app/.java-buildpack/tomcat/logs
 
+echo "/home/vcap/app"
+ls -l /home/vcap/app
+echo "/home/vcap"
+ls -l /home/vcap
+
 export JAVA_HOME="/home/vcap/app/.java-buildpack/open_jdk_jre"
 export LS_HEAP_SIZE=256m
 /home/vcap/app/logstash/bin/logstash agent -f /home/vcap/app/etc/logstash/conf.d &
