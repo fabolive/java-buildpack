1906885329bbb3b92702bde759760f058c6dbb52
FABIO A. OLIVEIRA <fabolive@us.ibm.com>
2015-10-27 18:05:40 -0400

    Make collectd part of the buildpack.

--- bin/logstash-install.sh
+++ bin/logstash-install.sh
@@ -6,6 +6,8 @@ my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 LOGSTASH_CONF_DIR=$build_dir/etc/logstash/conf.d
 
+mkdir -p $LOGSTASH_CONF_DIR
+
 cp $my_dir/logstash*.sh $build_dir
 
 pushd $build_dir
@@ -17,20 +19,13 @@ pushd $build_dir
   echo "Copying customized server.xml file to Tomcat distribution"
   cp $my_dir/../logstash-related-config/custom-server.xml .java-buildpack/tomcat/conf/server.xml
 
-  echo "Downloading collectd"
-  echo "Build dir = $build_dir"
-  ls -l $build_dir
-  which gcc
-  which make
-  wget https://collectd.org/files/collectd-5.4.2.tar.gz
-  tar xzf collectd-5.4.2.tar.gz
-#  cd collectd-5.4.2
-#  echo "Compiling collectd"
-#  ./configure --prefix=/root/test --exec-prefix=/root/test --bindir=/root/test/bin
+  echo "Unpacking and configuring collectd"
+  tar xzf $my_dir/collectd.tgz
+  rm $my_dir/collectd.tgz
+  cp $my_dir/../logstash-related-config/collectd.template etc/collectd.conf
 popd
 
 echo "Generating Logstash configuration"
-mkdir -p $LOGSTASH_CONF_DIR
 cp $my_dir/../logstash-related-config/logstash-tomcat.template $LOGSTASH_CONF_DIR/logstash-tomcat.conf
 sed -i s"/TOMCAT_SERVER_DIR/\/home\/vcap\/app\/\.java-buildpack\/tomcat/" $LOGSTASH_CONF_DIR/logstash-tomcat.conf
 sed -i s"/CA_TAG/$CA_TAG/" $LOGSTASH_CONF_DIR/logstash-tomcat.conf
@@ -40,7 +35,3 @@ sed -i s"/INDEX/$ES_INDEX/" $LOGSTASH_CONF_DIR/logstash-tomcat.conf
 
 cat $LOGSTASH_CONF_DIR/logstash-tomcat.conf
 
-echo "Generating collectd configuration"
-cp $my_dir/../logstash-related-config/collectd.template $build_dir/etc/collectd.conf
-
-cat $build_dir/etc/collectd.conf
--- bin/logstash-start.sh
+++ bin/logstash-start.sh
@@ -6,11 +6,12 @@ cat /home/vcap/app/.java-buildpack/tomcat/conf/server.xml
 
 echo "Tomcat Log Files:"
 ls -l /home/vcap/app/.java-buildpack/tomcat/logs
-
 echo "/home/vcap/app"
 ls -l /home/vcap/app
 echo "/home/vcap"
 ls -l /home/vcap
+
+/home/vcap/app/collectd/sbin/collectd -C /home/vcap/app/etc/collectd.conf
 
 export JAVA_HOME="/home/vcap/app/.java-buildpack/open_jdk_jre"
 export LS_HEAP_SIZE=256m
